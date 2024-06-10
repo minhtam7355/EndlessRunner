b907b944a38891fb670626e88ee6b2dedcd6fea4
Trinh Quoc Cuong <cuongvt203@gmail.com>
2024-06-10 11:18:42 +0700

    feat: Loading-and-Pause-v1
    Loading and Pause Menu version 1

---
+++ Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingManager : MonoBehaviour
+{
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Slider slider;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+        mainMenu.SetActive(false);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
