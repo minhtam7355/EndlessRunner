c9cd9a80749bceae66fba904661fd5f969fa48de
SE173544_NguyenDoTrungQuan1 <quanndtse173544@fpt.edu.vn>
2024-06-20 07:24:25 +0700

    Revert "merge"
    This reverts commit 769b5670528e7f2a6e6bf01baea0b3262b87e117, reversing
    changes made to bfa02e118b8f92b235bdf81b9774efb33fb341ad.

--- Assets/Scripts/CoinTrigger.cs
+++ Assets/Scripts/CoinTrigger.cs
@@ -5,44 +5,35 @@ using UnityEngine.UI;
 
 public class CoinTrigger : MonoBehaviour
 {
-	[SerializeField]
-	public Text coinText;
-	public float rotationSpeed = 100.0f;
-	private static int coinCount = 0;
+    [SerializeField]
+    public Text coinText;
+    public float rotationSpeed = 100.0f;
+    private static int coinCount = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that entered the trigger is tagged as "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Increment the coin count
+            coinCount++;
 
-	public AudioManager AudioManager;
+            // Update the coin text UI element
+            UpdateCoinText();
 
-	void Awake()
-	{
-		AudioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-	}
-	private void OnTriggerEnter(Collider other)
-	{
-		// Check if the object that entered the trigger is tagged as "Player"
-		if (other.CompareTag("Player"))
-		{
-			// Increment the coin count
-			coinCount++;
+            // Disable the coin object (make it disappear)
+            gameObject.SetActive(false);
 
-			// Update the coin text UI element
-			UpdateCoinText();
-
-			AudioManager.PlaySFX(AudioManager.CoinCollect);
-
-			// Disable the coin object (make it disappear)
-			gameObject.SetActive(false);
-
-			// Alternatively, you can return the coin to the pool if using pooling
-			// Example: CoinPool.Instance.ReturnCoin(gameObject);
-		}
-	}
-	void Update()
-	{
-		// Rotate the coin smoothly around the Y axis
-		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
-	}
-	private void UpdateCoinText()
-	{
-		coinText.text = "Coins: " + coinCount.ToString();
-	}
+            // Alternatively, you can return the coin to the pool if using pooling
+            // Example: CoinPool.Instance.ReturnCoin(gameObject);
+        }
+    }
+    void Update()
+    {
+        // Rotate the coin smoothly around the Y axis
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
+    }
+    private void UpdateCoinText()
+    {
+        coinText.text = "Coins: " + coinCount.ToString();
+    }
 }
--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -3,82 +3,71 @@ using UnityEngine;
 
 public class DeathTrigger : MonoBehaviour
 {
-	public bool IsDead { get; private set; }
-	public LogicScript logic;
-	[SerializeField]
-	private float maxZPosition = -8f; // Set your threshold value for instant death
+    public bool IsDead { get; private set; }
+    public LogicScript logic;
+    [SerializeField]
+    private float maxZPosition = -8f; // Set your threshold value for instant death
 
-	private Vector3 initialPosition; // To reset to the initial position
-	private Vector3 lastPosition; // To check if the player is stuck
-	private float stuckCheckDuration = 3f; // Time to wait before checking if stuck
-	private float stuckThreshold = 0.1f; // Distance threshold to consider the player stuck
-	private Animator _animator;
+    private Vector3 initialPosition; // To reset to the initial position
+    private Vector3 lastPosition; // To check if the player is stuck
+    private float stuckCheckDuration = 3f; // Time to wait before checking if stuck
+    private float stuckThreshold = 0.1f; // Distance threshold to consider the player stuck
+    private Animator _animator;
+	
 
+    private void Start()
+    {
+        IsDead = false;
+        initialPosition = transform.position; // Store the initial position
+        lastPosition = transform.position; // Initialize last known position
+        StartCoroutine(CheckIfStuck()); // Start checking if the player is stuck
+        _animator = GetComponent<Animator>();
+    }
 
-	public AudioManager AudioManager;
+    private void Update()
+    {
+        if (transform.position.z < maxZPosition)
+        {
+            Die();
+            logic.gameOver();
+        }
+    }
 
-	void Awake()
-	{
-		AudioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("KillZone"))
+        {
+            Die();
+            logic.gameOver();
+        }
+    }
 
-	private void Start()
-	{
-		IsDead = false;
-		initialPosition = transform.position; // Store the initial position
-		lastPosition = transform.position; // Initialize last known position
-		StartCoroutine(CheckIfStuck()); // Start checking if the player is stuck
-		_animator = GetComponent<Animator>();
-	}
+    
 
-	private void Update()
-	{
-		if (transform.position.z < maxZPosition)
-		{
-			Die();
-			logic.gameOver();
-		}
-	}
+    private IEnumerator CheckIfStuck()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(stuckCheckDuration);
 
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.CompareTag("KillZone"))
-		{
-			Die();
-			logic.gameOver();
-		}
-	}
+            float distanceMoved = Vector3.Distance(lastPosition, transform.position);
 
+            // Check if the player is stuck and not below the death threshold
+            if (distanceMoved < stuckThreshold && transform.position.z >= maxZPosition)
+            {
+                ResetPosition();
+            }
 
-
-	private IEnumerator CheckIfStuck()
-	{
-		while (true)
-		{
-			yield return new WaitForSeconds(stuckCheckDuration);
-
-			float distanceMoved = Vector3.Distance(lastPosition, transform.position);
-
-			// Check if the player is stuck and not below the death threshold
-			if (distanceMoved < stuckThreshold && transform.position.z >= maxZPosition)
-			{
-				ResetPosition();
-			}
-
-			lastPosition = transform.position;
-		}
-	}
-	public void Die()
+            lastPosition = transform.position;
+        }
+    }
+    public void Die()
 	{
 		if (IsDead) return;
 
 		IsDead = true;
 
 		Debug.Log("Player died!");
-
-		AudioManager.StopBackgroundMusic();
-
-		AudioManager.PlaySFX(AudioManager.Death);
 
 		// Play death animation
 		_animator.Play("Death");
@@ -99,17 +88,17 @@ public class DeathTrigger : MonoBehaviour
 		logic.gameOver();
 	}
 
-	private void ResetPosition()
-	{
-		Debug.Log("Player is stuck. Resetting position.");
-		Vector3 newPosition = transform.position; // Get the current position
-		newPosition.z = initialPosition.z; // Reset only the z value
-		transform.position = newPosition; // Set the new position
-													 // Optionally, reset other states or variables as needed
-	}
+    private void ResetPosition()
+    {
+        Debug.Log("Player is stuck. Resetting position.");
+        Vector3 newPosition = transform.position; // Get the current position
+        newPosition.z = initialPosition.z; // Reset only the z value
+        transform.position = newPosition; // Set the new position
+                                          // Optionally, reset other states or variables as needed
+    }
+	
 
+	
 
-
-
-
+	
 }
